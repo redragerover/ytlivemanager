65a45a566b5666d89143d7926435300b74c47afd
redragerover <red_rage_rover@protonmail.com>
2022-09-04 13:33:40 -0400

    add identifier to function

--- statehandler.js
+++ statehandler.js
@@ -1,28 +1,24 @@
-import { exec } from  "child_process";
-import dotenv from "dotenv"
-import chalk from "chalk"
-import { getLiveVideoURLFromChannelID} from "./urlUtils.js"
-import { handleDoubleCheck, handleStreameIsRemainingOnline, handleStreamerIsOn } from "./ytLiveState.js"
+import dotenv from "dotenv";
+import chalk from "chalk";
+import { getLiveVideoURLFromChannelID } from "./urlUtils.js";
+import {
+  handleDoubleCheck,
+  handleStreameIsRemainingOnline,
+  handleStreamerIsOn,
+} from "./ytLiveState.js";
 
-const toSeconds = seconds => seconds * 1000
-const toMinutes = minutes => 60_000 * minutes
+const toSeconds = (seconds) => seconds * 1000;
 
-const nodeArguments = process.argv
-dotenv.config()
-const ytChannelId = process.env.ytChannelId
-const saveFilePath = process.env.saveFilePath
+dotenv.config();
 
-const pollingIntervalTimer = toSeconds(49)
-const timeToDelayCheck = toSeconds(40)
-const getStreamStatus= (streamIsOnline)=>{
-	if(streamIsOnline){
-		console.log(chalk.white.bgBlack("Stream is online!"))
-	} 
-	else {
-		console.log(chalk.white.bgRed("Stream offline"))
-	}
-
-}
+const pollingIntervalTimer = toSeconds(49);
+const getStreamStatus = (streamIsOnline) => {
+  if (streamIsOnline) {
+    console.log(chalk.white.bgBlack("Stream is online!"));
+  } else {
+    console.log(chalk.white.bgRed("Stream offline"));
+  }
+};
 
 /**
  * handleYouTubePoll.
@@ -30,54 +26,67 @@ const getStreamStatus= (streamIsOnline)=>{
  * @param {function} streamGoesOffline - handles what happens when stream goes offline
  * @param {function} streamToLive - handles when stream goes live
  */
-export const handleYouTubePoll = ({streamGoesOffline, streamToLive}) => {
-	console.log(chalk.green("ytlivemanager has started"))
-	const state = {
-		streamerIsOn: false,
-		streamIsAlreadyOnline: nodeArguments.includes("skip"),
-		doubleCheckIfOffline: false,
-		intervalCounter: 0,
-		setStreamerIsOn(val) {
-			state.streamerIsOn = val
-		},
-		setStreamIsAlreadyOnline(val) {
-			state.streamIsAlreadyOnline = val
-		},
-		setDoubleCheckIfOffline(val) {
-			console.log(state.doubleCheckIfOffline)
-			state.doubleCheckIfOffline = val
-		},
-	}	
-	
-	const handleInterval = async () => {
-		if(state.intervalCounter > 0){
-			state.intervalCounter = 0
-			console.clear()
-			getStreamStatus(state.streamIsAlreadyOnline)
-		}
+export const handleYouTubePoll = ({
+  identifier,
+  streamGoesOffline,
+  streamToLive,
+}) => {
+  if (!identifier) {
+    console.log("identifier undefined");
+  }
+  const ytChannelId = identifier;
 
+  console.log(chalk.green("ytlivemanager has started"));
+  const state = {
+    streamerIsOn: false,
+    streamIsAlreadyOnline: false,
+    doubleCheckIfOffline: false,
+    intervalCounter: 0,
+    setStreamerIsOn(val) {
+      state.streamerIsOn = val;
+    },
+    setStreamIsAlreadyOnline(val) {
+      state.streamIsAlreadyOnline = val;
+    },
+    setDoubleCheckIfOffline(val) {
+      console.log(state.doubleCheckIfOffline);
+      state.doubleCheckIfOffline = val;
+    },
+  };
 
-		state.intervalCounter++
+  const handleInterval = async () => {
+    if (state.intervalCounter > 0) {
+      state.intervalCounter = 0;
+      console.clear();
+      getStreamStatus(state.streamIsAlreadyOnline);
+    }
 
-		await getLiveVideoURLFromChannelID(ytChannelId).then(({ isStreaming, canonicalURL }) => {
+    state.intervalCounter++;
 
-			if(state.streamerIsOn){
-				return;
-			}
-			handleDoubleCheck(state, streamGoesOffline)
-			if(state.doubleCheckIfOffline){
-				return
-			}
-			handleStreameIsRemainingOnline(state, isStreaming)
-			if(state.streamIsAlreadyOnline){
-				return
-			}
+    await getLiveVideoURLFromChannelID(ytChannelId)
+      .then(({ isStreaming, canonicalURL }) => {
+        if (state.streamerIsOn) {
+          return;
+        }
+        handleDoubleCheck(state, streamGoesOffline);
+        if (state.doubleCheckIfOffline) {
+          return;
+        }
+        handleStreameIsRemainingOnline(state, isStreaming);
+        if (state.streamIsAlreadyOnline) {
+          return;
+        }
 
-			handleStreamerIsOn(state,  streamToLive , isStreaming, 1000 * 20)
-			return;
-		}).catch(err => console.log(err))
-	}
+        handleStreamerIsOn(
+          state,
+          () => streamToLive(canonicalURL),
+          isStreaming,
+          1000 * 20
+        );
+        return;
+      })
+      .catch((err) => console.log(err));
+  };
 
-	const interval = setInterval(handleInterval, pollingIntervalTimer)
-
-}
+  const interval = setInterval(handleInterval, pollingIntervalTimer);
+};
--- ytLiveState.js
+++ ytLiveState.js
@@ -1,71 +1,80 @@
 /**
- * 
- * @param {object} state 
- * @param {function} actionWhenDoubleCheckIsTrue 
- * @returns 
+ *
+ * @param {object} state
+ * @param {function} actionWhenDoubleCheckIsTrue
+ * @returns
  */
 const handleDoubleCheck = (state, actionWhenDoubleCheckIsTrue, isStreaming) => {
+  const { setDoubleCheckIfOffline, setStreamIsAlreadyOnline } = state;
 
-    const { setDoubleCheckIfOffline, setStreamIsAlreadyOnline } = state
+  if (!state.doubleCheckIfOffline) {
+    return;
+  }
 
-
-    if (!state.doubleCheckIfOffline) {
-        return
-    }
-
-    if (isStreaming) {
-        setStreamIsAlreadyOnline(true)
-        setDoubleCheckIfOffline(false)
-        return;
-    }
-    if (!isStreaming) {
-        setDoubleCheckIfOffline(false)
-        setStreamIsAlreadyOnline(false)
-        actionWhenDoubleCheckIsTrue()
-        console.log("stream permanently offline")
-    }
-}
+  if (isStreaming) {
+    setStreamIsAlreadyOnline(true);
+    setDoubleCheckIfOffline(false);
+    return;
+  }
+  if (!isStreaming) {
+    setDoubleCheckIfOffline(false);
+    setStreamIsAlreadyOnline(false);
+    actionWhenDoubleCheckIsTrue();
+    console.log("stream permanently offline");
+  }
+};
 
 /**
- * 
- * @param {object} state 
- * @param {function} actionWhenStreamIsOn 
- * @returns 
+ *
+ * @param {object} state
+ * @param {function} actionWhenStreamIsOn
+ * @returns
  */
-const handleStreamerIsOn = (state, actionWhenStreamIsOn, isStreaming, timeToDelayCheck) => {
-
-    const { setStreamIsAlreadyOnline, setStreamerIsOn } = state
-    const sendMessageToEveryone = isStreaming && !state.streamIsAlreadyOnline && !state.doubleCheckIfOffline
-    if (sendMessageToEveryone) {
-        //send message to everyone
-        setStreamIsAlreadyOnline(true)
-        setStreamerIsOn(true)
-        actionWhenStreamIsOn()
-        setTimeout(() => setStreamerIsOn(false), timeToDelayCheck)
-        return;
-    }
-    if (!sendMessageToEveryone) {
-//        console.log(`stream is online: ${state.streamIsAlreadyOnline}`)
-        return
-    }
-}
+const handleStreamerIsOn = (
+  state,
+  actionWhenStreamIsOn,
+  isStreaming,
+  timeToDelayCheck
+) => {
+  const { setStreamIsAlreadyOnline, setStreamerIsOn } = state;
+  const onlineAndReadyToPreformActions =
+    isStreaming && !state.streamIsAlreadyOnline && !state.doubleCheckIfOffline;
+  if (onlineAndReadyToPreformActions) {
+    //send message to everyone
+    setStreamIsAlreadyOnline(true);
+    setStreamerIsOn(true);
+    actionWhenStreamIsOn();
+    setTimeout(() => setStreamerIsOn(false), timeToDelayCheck);
+    return;
+  }
+  if (!onlineAndReadyToPreformActions) {
+    return;
+  }
+};
 
 /**
- * 
- * @param {object} state 
- * @returns 
+ *
+ * @param {object} state
+ * @returns
  */
 const handleStreameIsRemainingOnline = (state, isStreaming) => {
+  const { setDoubleCheckIfOffline, setStreamIsAlreadyOnline } = state;
+  if (isStreaming && state.streamIsAlreadyOnline) {
+    return;
+  }
+  if (
+    !isStreaming &&
+    !state.doubleCheckIfOffline &&
+    state.streamIsAlreadyOnline
+  ) {
+    setDoubleCheckIfOffline(true);
+    setStreamIsAlreadyOnline(false);
+    return;
+  }
+};
 
-    const { setDoubleCheckIfOffline, setStreamIsAlreadyOnline } = state
-    if (isStreaming && state.streamIsAlreadyOnline) {
-        return
-    }
-    if (!isStreaming && !state.doubleCheckIfOffline && state.streamIsAlreadyOnline) {
-        setDoubleCheckIfOffline(true)
-        setStreamIsAlreadyOnline(false)
-        return;
-    }
-}
-
-export { handleStreameIsRemainingOnline, handleStreamerIsOn, handleDoubleCheck }
+export {
+  handleStreameIsRemainingOnline,
+  handleStreamerIsOn,
+  handleDoubleCheck,
+};
