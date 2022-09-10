8fd7fccd5801170e894aef85cc5c3194bb0f9576
redragerover <red_rage_rover@protonmail.com>
2022-09-09 20:45:19 -0400

    Add tests to ytLiveState

---
+++ tests/ytLiveState.test.js
@@ -0,0 +1,62 @@
+import { jest } from "@jest/globals";
+
+import { handleDoubleCheck } from "../ytLiveState";
+describe("handleDoubleCheck", () => {
+  it("should not call functions when doubleCheckIfOffline is false", () => {
+    const actionWhenDoubleCheckIsTrue = jest.fn();
+    const setDoubleCheckIfOffline = jest.fn();
+    const setStreamIsAlreadyOnline = jest.fn();
+    const state = {
+      doubleCheckIfOffline: false,
+      setDoubleCheckIfOffline,
+      setStreamIsAlreadyOnline,
+    };
+    const isStreaming = false;
+    handleDoubleCheck(state, actionWhenDoubleCheckIsTrue, isStreaming);
+    expect(actionWhenDoubleCheckIsTrue).not.toHaveBeenCalled();
+    expect(setDoubleCheckIfOffline).not.toHaveBeenCalled();
+    expect(setStreamIsAlreadyOnline).not.toHaveBeenCalled();
+  });
+  it("doubleCheckIfOffline is true and if isStreaming, then setStreamIsAlreadyOnline set to true doubleCheckIfOffline set to false", () => {
+    const actionWhenDoubleCheckIsTrue = jest.fn();
+    const setDoubleCheckIfOffline = jest.fn();
+    const setStreamIsAlreadyOnline = jest.fn();
+    const state = {
+      doubleCheckIfOffline: true,
+      setDoubleCheckIfOffline,
+      setStreamIsAlreadyOnline,
+    };
+    const isStreaming = true;
+
+    handleDoubleCheck(state, actionWhenDoubleCheckIsTrue, isStreaming);
+
+    expect(actionWhenDoubleCheckIsTrue).not.toHaveBeenCalled();
+    expect(setDoubleCheckIfOffline).toHaveBeenCalledWith(false);
+    expect(setStreamIsAlreadyOnline).toHaveBeenCalledWith(true);
+    expect(setDoubleCheckIfOffline).toHaveBeenCalledTimes(1);
+    expect(setStreamIsAlreadyOnline).toHaveBeenCalledTimes(1);
+  });
+
+  it("doubleCheckIfOffline is true and if !isStreaming, then actionWhenDoubleCheckIsTrue is called, setDoubleCheckIfOffline  and setStreamIsAlreadyOnline to false", () => {
+    {
+      const actionWhenDoubleCheckIsTrue = jest.fn();
+      const setDoubleCheckIfOffline = jest.fn();
+      const setStreamIsAlreadyOnline = jest.fn();
+      const state = {
+        doubleCheckIfOffline: true,
+        setDoubleCheckIfOffline,
+        setStreamIsAlreadyOnline,
+      };
+
+      const isStreaming = false;
+
+      handleDoubleCheck(state, actionWhenDoubleCheckIsTrue, isStreaming);
+
+      expect(setDoubleCheckIfOffline).toHaveBeenCalledWith(false);
+      expect(setStreamIsAlreadyOnline).toHaveBeenCalledWith(false);
+      expect(setDoubleCheckIfOffline).toHaveBeenCalledTimes(1);
+      expect(setStreamIsAlreadyOnline).toHaveBeenCalledTimes(1);
+      expect(actionWhenDoubleCheckIsTrue).toHaveBeenCalled();
+    }
+  });
+});
